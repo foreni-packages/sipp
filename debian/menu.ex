e351503030820ef07a379eef5b9ff855d89e9b88
Devon Kearns <dookie@kali.org>
2012-12-29 07:03:51 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sipp):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="sipp" command="/usr/bin/sipp"
--- debian/sipp.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sipp package
-#
-0 4	* * *	root	[ -x /usr/bin/sipp_maintenance ] && /usr/bin/sipp_maintenance
